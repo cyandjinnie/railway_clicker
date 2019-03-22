0972ad1fc36dcb645d4d5d5660a54c7a61d5b5bd
Maksimov Igor <igormaximow@gmail.com>
2019-03-23 00:17:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from Application import Application
+from Database import Database
+import Director
+from pygame import *
+
+def main():
+    db = Database()
+    db['screen_width'] = 800
+    db['screen_height'] = 450
+    app = Application("MyApp")
+    app.create_window(db['screen_width'], db['screen_height'], "MyWindow")
+    return app.run()
+
+if __name__ == "__main__":
+    main()
